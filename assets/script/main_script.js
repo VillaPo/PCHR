18c0ebeeff1e7f7a6ddb692f0b43e32506de481d
Virtual Times <66945396+VillaPo@users.noreply.github.com>
2020-10-27 10:41:39 +0200

    Add files via upload

---
+++ assets/script/main_script.js
@@ -0,0 +1,5 @@
+﻿function toggleMobileMenu() {
+    document.querySelector('.menu-bar').classList.toggle('active')
+    //document.querySelector('.menu-bar__item').classList.toggle('active')
+    document.querySelector('.menu-bar--mobile').classList.toggle('active')
+}
